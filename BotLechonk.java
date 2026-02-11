ba6402c606fcffba2acbbd5de7bc9de1de5dd8cd
AdaXiang <147667984+AdaXiang@users.noreply.github.com>
2026-02-11 19:00:16 +0100

    Commit inicial

---
+++ BotLechonk.java
@@ -0,0 +1,28 @@
+package BotLechonk;
+import java.io.*;
+import java.util.*;
+
+public class BotLechonk {
+    
+    public static void main (String [] args) throws Exception {
+        
+       
+    }
+
+    public void listIt (){
+
+    }
+
+    public void contPalabras (){
+
+    }
+
+    public void salvarObjeto (){
+
+    }
+
+    public void cargarObjeto (){
+
+    }
+
+}
